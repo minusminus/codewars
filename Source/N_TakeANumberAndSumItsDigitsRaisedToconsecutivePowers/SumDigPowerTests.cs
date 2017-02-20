5717bd6bc104650294c6e6a62a66f37d26091505
Michał Śmiechowski <Michal.Smiechowski@DSA.Polska>
2017-02-20 14:14:44 +0100

    init Take a Number And Sum Its Digits Raised To The Consecutive Powers And ....¡Eureka!!

---
+++ Source/N_TakeANumberAndSumItsDigitsRaisedToconsecutivePowers/SumDigPowerTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using NUnit.Framework;
+using TakeANumberAndSumItsDigitsRaisedToconsecutivePowers;
+
+namespace N_TakeANumberAndSumItsDigitsRaisedToconsecutivePowers
+{
+    [TestFixture]
+    public class SumDigPowerTests
+    {
+        private static string Array2String(long[] list)
+        {
+            return "[" + string.Join(", ", list) + "]";
+        }
+        private static void testing(long a, long b, long[] res)
+        {
+            Assert.AreEqual(Array2String(res),
+                            Array2String(SumDigPower.SumDigPow(a, b)));
+        }
+
+        [Test]
+        public static void test1()
+        {
+            Console.WriteLine("Basic Tests SumDigPow");
+            testing(1, 10, new long[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 });
+            testing(1, 100, new long[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 89 });
+            testing(10, 100, new long[] { 89 });
+            testing(90, 100, new long[] { });
+            testing(90, 150, new long[] { 135 });
+            testing(50, 150, new long[] { 89, 135 });
+            testing(10, 150, new long[] { 89, 135 });
+
+        }
+    }
+}
---
+++ Source/TakeANumberAndSumItsDigitsRaisedToconsecutivePowers/SumDigPower.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TakeANumberAndSumItsDigitsRaisedToconsecutivePowers
+{
+    public class SumDigPower
+    {
+        public static long[] SumDigPow(long a, long b)
+        {
+            return new long[0];
+        }
+    }
+}
