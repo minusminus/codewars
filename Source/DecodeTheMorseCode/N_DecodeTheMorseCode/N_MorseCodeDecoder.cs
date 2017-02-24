7749f6e906c47212da35032f36d9cc0ac6e2c650
Michał Śmiechowski <Michal.Smiechowski@DSA.Polska>
2017-02-24 22:04:12 +0100

    initial morse code 1

---
+++ Source/DecodeTheMorseCode/N_DecodeTheMorseCode/N_MorseCodeDecoder.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using NUnit.Framework;
+using Shouldly;
+using DecodeTheMorseCode;
+
+namespace N_DecodeTheMorseCode
+{
+    [TestFixture]
+    public class N_MorseCodeDecoder
+    {
+        private static MorseCodeDecoder _test = new MorseCodeDecoder();
+
+        [Test]
+        public void OriginalTest()
+        {
+            _test.Decode(".... . -.--   .--- ..- -.. .").ShouldBe("HEY JUDE");
+        }
+    }
+}
