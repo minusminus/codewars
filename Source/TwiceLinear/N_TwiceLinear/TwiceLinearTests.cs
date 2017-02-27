a59833da94046a2d71a8ac22155980beded3646a
Michał Śmiechowski <Michal.Smiechowski@DSA.Polska>
2017-02-27 16:59:30 +0100

    bruteforce test

--- Source/TwiceLinear/N_TwiceLinear/TwiceLinearTests.cs
+++ Source/TwiceLinear/N_TwiceLinear/TwiceLinearTests.cs
@@ -11,16 +11,34 @@ namespace N_TwiceLinear
 
         private void doTest(int n, int expected)
         {
-            _test.DblLinear(n).ShouldBe(expected);
+            //_test.DblLinear(n).ShouldBe(expected);
+            _test.DblLinearBruteforce(n).ShouldBe(expected);
         }
 
         [Test]
         public void TestKata()
         {
-            doTest(10, 27);
+            doTest(10, 22);
             doTest(20, 57);
             doTest(30, 91);
             doTest(40, 175);
+        }
+
+        [Test]
+        public void TestFirst11()
+        {
+            doTest(0, 1);
+            doTest(1, 3);
+            doTest(2, 4);
+            doTest(3, 7);
+            doTest(4, 9);
+            doTest(5, 10);
+            doTest(6, 13);
+            doTest(7, 15);
+            doTest(8, 19);
+            doTest(9, 21);
+            doTest(10, 22);
+            doTest(11, 27);
         }
     }
 }
--- Source/TwiceLinear/TwiceLinear/TwiceLinear.cs
+++ Source/TwiceLinear/TwiceLinear/TwiceLinear.cs
@@ -26,12 +26,52 @@ namespace TwiceLinear
             return (y - 1f)/3f;
         }
 
+        private int countElements(int n)
+        {
+            return n + (int)Math.Floor( f2inv(f1(n)) );
+        }
+
+        public int DblLinearBruteforce(int n)
+        {
+            List<int> tbl = new List<int>();
+            tbl.Add(1);
+            for (int i = 0; i < n; i++)
+            {
+                int v = f1(tbl[i]);
+                if(!tbl.Contains(v)) tbl.Add(v);
+                v = f2(tbl[i]);
+                if (!tbl.Contains(v)) tbl.Add(v);
+            }
+            tbl.Sort();
+            return tbl[n];
+        }
+
         public int DblLinear(int n)
         {
             if (n == 0) return 1;
-            int res = 0;
 
-            return res;
+            int left = 1;
+            int right = countElements(n);
+            while (right - left > 1)
+            {
+                int half = (right - left)/2 + left;
+                int cnt = countElements(half);
+                if (cnt > n)
+                    right = half;
+                else
+                    left = half;
+            }
+
+            int cntl = countElements(left);
+            int cntr = countElements(right);
+            List<int> tbl = new List<int>();
+            for (int i = cntl; i <= cntr; i++)
+            {
+                tbl.Add(f1(i));
+                tbl.Add(f2(i));
+            }
+            tbl.Sort();
+            return tbl[n - cntl];
         }
     }
 }
