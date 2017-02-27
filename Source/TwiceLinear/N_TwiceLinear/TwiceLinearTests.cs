ca3491abe7eb70de91c48741120bd61df9fac59a
Michał Śmiechowski <Michal.Smiechowski@DSA.Polska>
2017-02-27 15:54:02 +0100

    init tests, init funcs

--- Source/TwiceLinear/N_TwiceLinear/TwiceLinearTests.cs
+++ Source/TwiceLinear/N_TwiceLinear/TwiceLinearTests.cs
@@ -1,14 +1,26 @@
 ﻿using System;
 using NUnit.Framework;
+using Shouldly;
 
 namespace N_TwiceLinear
 {
     [TestFixture]
     public class TwiceLinearTests
     {
-        [Test]
-        public void TestMethod1()
+        private readonly TwiceLinear.TwiceLinear _test = new TwiceLinear.TwiceLinear();
+
+        private void doTest(int n, int expected)
         {
+            _test.DblLinear(n).ShouldBe(expected);
+        }
+
+        [Test]
+        public void TestKata()
+        {
+            doTest(10, 27);
+            doTest(20, 57);
+            doTest(30, 91);
+            doTest(40, 175);
         }
     }
 }
--- Source/TwiceLinear/TwiceLinear/TwiceLinear.cs
+++ Source/TwiceLinear/TwiceLinear/TwiceLinear.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿/*
+ * https://www.codewars.com/kata/twice-linear
+ */
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +11,27 @@ namespace TwiceLinear
 {
     public class TwiceLinear
     {
+        private int f1(int x)
+        {
+            return 2*x + 1;
+        }
+
+        private int f2(int x)
+        {
+            return 3*x + 1;
+        }
+
+        private float f2inv(int y)
+        {
+            return (y - 1f)/3f;
+        }
+
+        public int DblLinear(int n)
+        {
+            if (n == 0) return 1;
+            int res = 0;
+
+            return res;
+        }
     }
 }
