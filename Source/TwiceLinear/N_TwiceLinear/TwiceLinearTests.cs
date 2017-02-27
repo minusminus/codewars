bfe5cbf74006335b628ef165e9f507e64b457860
Michał Śmiechowski <Michal.Smiechowski@DSA.Polska>
2017-02-27 15:45:28 +0100

    twicelinear init

---
+++ Source/TwiceLinear/N_TwiceLinear/TwiceLinearTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace N_TwiceLinear
+{
+    [TestFixture]
+    public class TwiceLinearTests
+    {
+        [Test]
+        public void TestMethod1()
+        {
+        }
+    }
+}
