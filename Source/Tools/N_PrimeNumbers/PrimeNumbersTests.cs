99b6a31936c1c1681a19c91cd6a4fc7e62b59382
Michał Śmiechowski <Michal.Smiechowski@DSA.Polska>
2017-03-06 14:16:13 +0100

    primenumbers init

---
+++ Source/Tools/N_PrimeNumbers/PrimeNumbersTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace N_PrimeNumbers
+{
+    [TestFixture]
+    public class PrimeNumbersTests
+    {
+        [Test]
+        public void TestMethod1()
+        {
+        }
+    }
+}
