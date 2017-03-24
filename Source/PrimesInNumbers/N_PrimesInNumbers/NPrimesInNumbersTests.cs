f3935c015639b8ab9d1b42978f9ecb108025afef
michal.smiechowski <michal.smiechowski@PC-MSM-W7>
2017-03-24 15:58:30 +0100

    Primes In Numbers init...

---
+++ Source/PrimesInNumbers/N_PrimesInNumbers/NPrimesInNumbersTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace N_PrimesInNumbers
+{
+    [TestFixture]
+    public class NPrimesInNumbersTests
+    {
+        [Test]
+        public void TestMethod1()
+        {
+        }
+    }
+}
