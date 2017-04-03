02293eda1d3883aa10ccc23990dea4f260775b89
michal.smiechowski <michal.smiechowski@PC-MSM-W7>
2017-04-03 14:09:23 +0200

    CountdownLongestWord init

---
+++ Source/CountdownLongestWord/CountdownLongestWord/CLWWordsDict.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CountdownLongestWord
+{
+    public class CLWWordsDict
+    {
+        /// <summary>
+        /// slownik slow dla posortowanych liter
+        /// </summary>
+        public Dictionary<string, List<string>> WordsDict { get; } = new Dictionary<string, List<string>>();
+        /// <summary>
+        /// licznik slow zaczynajacych sie od danej litery (dla slow z posortowanych liter)
+        /// </summary>
+        public Dictionary<char, int> WordsStartsWith { get; } = new Dictionary<char, int>();
+
+        public char[] StringToSortedLetters(string s)
+        {
+            char[] res = s.ToCharArray();
+            Array.Sort(res);
+            return res;
+        }
+
+        public string LettersToString(char[] letters)
+        {
+            return letters.ToString();
+        }
+
+        private void PrepareWordsDict(string[] words)
+        {
+            foreach (string word in words)
+            {
+                string w = word.ToUpper();
+                string s = LettersToString(StringToSortedLetters(w));
+                    //slownik slow dla posortowanych liter
+                List<string> ls;
+                if (WordsDict.TryGetValue(s, out ls))
+                {
+                    ls.Add(word);
+                }
+                else
+                {
+                    ls = new List<string>() { w };
+                    WordsDict.Add(s, ls);
+                }
+                    //licznik slow zaczynajacych sie od okreslonej litery
+                int i;
+                if (WordsStartsWith.TryGetValue(s[0], out i))
+                {
+                    WordsStartsWith[s[0]] = i + 1;
+                }
+                else
+                {
+                    WordsStartsWith.Add(s[0], 1);
+                }
+            }
+        }
+    }
+}
---
+++ Source/CountdownLongestWord/NCountdownLongestWord/NCountdownLongestWord.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace NCountdownLongestWord
+{
+    [TestFixture]
+    public class NCountdownLongestWord
+    {
+        private readonly CountdownLongestWord.CountdownLongestWord _pobj = new CountdownLongestWord.CountdownLongestWord();
+
+        [Test]
+        public void TestTryGetValue()
+        {
+            Dictionary<string, List<string>> dict = new Dictionary<string, List<string>>();
+            dict.Add("abcd", new List<string>() {"abcd", "bacd"});
+
+            foreach (List<string> value in dict.Values)
+                foreach (string s in value)
+                    Console.Write($"{s},");
+            Console.WriteLine();
+
+            List<string> ls;
+            if (dict.TryGetValue("abcd", out ls))
+            {
+                ls.Add("abdc");
+            }
+
+            foreach (List<string> value in dict.Values)
+                foreach (string s in value)
+                    Console.Write($"{s},");
+            Console.WriteLine();
+
+            if (dict.TryGetValue("abcd", out ls))
+            {
+                ls.Add("badc");
+            }
+
+            foreach (List<string> value in dict.Values)
+                foreach (string s in value)
+                    Console.Write($"{s},");
+            Console.WriteLine();
+        }
+
+        [Test]
+        public void TestTryGetInt()
+        {
+            Dictionary<char, int> dict = new Dictionary<char, int>();
+            dict.Add('1', 1);
+            int i;
+
+            foreach (KeyValuePair<char, int> pair in dict)
+                Console.Write($"{pair.Key}={pair.Value},");
+            Console.WriteLine();
+
+            if (dict.TryGetValue('1', out i)) i = 10;
+            foreach (KeyValuePair<char, int> pair in dict)
+                Console.Write($"{pair.Key}={pair.Value},");
+            Console.WriteLine();
+
+            if (dict.TryGetValue('1', out i)) dict['1'] = 10;
+            foreach (KeyValuePair<char, int> pair in dict)
+                Console.Write($"{pair.Key}={pair.Value},");
+            Console.WriteLine();
+
+            dict['2'] = 2;
+            foreach (KeyValuePair<char, int> pair in dict)
+                Console.Write($"{pair.Key}={pair.Value},");
+            Console.WriteLine();
+
+            //dict['3'] = dict['3'] + 2;
+            //foreach (KeyValuePair<char, int> pair in dict)
+            //    Console.Write($"{pair.Key}={pair.Value},");
+            //Console.WriteLine();
+        }
+    }
+}
