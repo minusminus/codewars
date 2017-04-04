777683ef662cbf50dcd73deb89c5f5d50d5a6602
michal.smiechowski <michal.smiechowski@PC-MSM-W7>
2017-04-04 16:48:36 +0200

    CountdownLongestWord init

--- Source/CountdownLongestWord/CountdownLongestWord/CountdownLongestWord.cs
+++ Source/CountdownLongestWord/CountdownLongestWord/CountdownLongestWord.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,18 @@ namespace CountdownLongestWord
 {
     public class CountdownLongestWord
     {
+        private readonly string[] _words = File.ReadAllLines(@"d:\projects\codewars_github\tools---numbertheory\Source\CountdownLongestWord\google-10000-english-no-swears.txt");
+
+        private readonly CLWWordsDict _clw = new CLWWordsDict();
+
+        public string[] LongestWord(string letters)
+        {
+            if (String.IsNullOrEmpty(letters)) return null;
+            if (_clw.WordsDict.Count == 0) _clw.PrepareWordsDict(_words);
+
+            char[] splitted = _clw.StringToSortedLetters(letters);
+                
+            return null;
+        }
     }
 }
