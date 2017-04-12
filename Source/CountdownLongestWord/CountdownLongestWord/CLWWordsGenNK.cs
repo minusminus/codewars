f2653de04624f8444bf6ed959541fd5ba88fbce8
michal.smiechowski <michal.smiechowski@PC-MSM-W7>
2017-04-12 17:04:03 +0200

    words gen tests

--- Source/CountdownLongestWord/CountdownLongestWord/CLWWordsGenNK.cs
+++ Source/CountdownLongestWord/CountdownLongestWord/CLWWordsGenNK.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -27,28 +29,58 @@ namespace CountdownLongestWord
 
         public void GenerateValues(int k, Action<string> processString )
         {
-            for (int i = 0; i < k; i++)
-                _idx[i] = i;
-            int maxpos = _sbase.Length - 1;
-            int cpos = k - 1;
+            for (int i = 0; i < _idx.Length; i++) _idx[i] = 0;
+            IntGenValues(k, k, _sbase.Length-1, processString);
+            //for (int i = 0; i < k; i++)
+            //    _idx[i] = i;
+            //int maxpos = _sbase.Length - 1;
+            //int cpos = k - 1;
 
-            while (cpos >= 0)
+            //string s = GetMaskedString(k);  //pierwszy geenrowany ciag
+            //processString(s);
+            //while (cpos >= 0)
+            //{
+            //    while ((cpos >= 0) && (_idx[cpos] == maxpos))
+            //    {
+            //        //jezeli ostatni dotarl do swojej ostatniej pozycji to cofamy sie w lewo
+            //        cpos--;
+            //        maxpos--;
+            //    }
+            //    for (int il = cpos; il >= 0; il--) //przesuniecie wszystkich flag o 1 w prawo
+            //    {
+            //        _idx[il]++;
+            //        s = GetMaskedString(k);
+            //        processString(s);   
+            //    }
+            //    for (int il = cpos - 1; il >= 0; il--) //cofniecie flag oprocz ostatniej, tylko te dla ktorych nastepna nie jest na koncu
+            //        if (_idx[il + 1] < maxpos)
+            //            _idx[il]--;
+            //}
+        }
+
+        private void IntGenValues(int totalk, int k, int maxpos, Action<string> processString)
+        {
+            if (k == 1)
             {
-                while ((cpos >= 0) && (_idx[cpos] == maxpos))
+                if (totalk == 1) maxpos++;
+                for (int i = 0; i < maxpos; i++)
                 {
-                    //jezeli ostatni dotarl do swojej ostatniej pozycji to cofamy sie w lewo
-                    cpos--;
-                    maxpos--;
-                }
-                for (int il = cpos; il >= 0; il--) //przesuniecie wszystkich flag o 1 w prawo
-                {
-                    string s = GetMaskedString(k);
-                    //tu przetworzenie wygenerowanego stringa
+                    _idx[0] = i;
+                    string s = GetMaskedString(totalk);
                     processString(s);
-                    _idx[il]++;
                 }
-                for (int il = cpos - 1; il >= 0; il--) //cofniecie flag oprocz ostatniej
-                    _idx[il]--;
+            }
+            else
+            {
+                for (int i = 0; i < k; i++) _idx[i] = i;
+                string s = GetMaskedString(totalk);
+                processString(s);
+
+                while (_idx[k-1] < maxpos)
+                {
+                    _idx[k-1]++;
+                    IntGenValues(totalk, k - 1, _idx[k-1], processString);
+                }
             }
         }
     }
--- Source/CountdownLongestWord/NCountdownLongestWord/NCLWWordsGenNK.cs
+++ Source/CountdownLongestWord/NCountdownLongestWord/NCLWWordsGenNK.cs
@@ -16,8 +16,8 @@ namespace NCountdownLongestWord
             
             _pobj.GenerateValues(k, (s)=> {testres.Add(s);});
 
-            testres.Sort();
-            expected.Sort();
+            //testres.Sort();
+            //expected.Sort();
             testres.ShouldBe(expected);
         }
 
@@ -25,6 +25,30 @@ namespace NCountdownLongestWord
         public void Test_5of3()
         {
             DoTestNofK("abcde", 3, new List<string>() {"abc", "abd", "acd", "bcd", "abe", "ace", "bce", "ade", "bde", "cde"});
+        }
+
+        [Test]
+        public void Test_3of3()
+        {
+            DoTestNofK("abc", 3, new List<string>() { "abc" });
+        }
+
+        [Test]
+        public void Test_3of1()
+        {
+            DoTestNofK("abc", 1, new List<string>() { "a", "b", "c" });
+        }
+
+        [Test]
+        public void Test_5of2()
+        {
+            DoTestNofK("abcde", 2, new List<string>() { "ab", "ac", "bc", "ad", "bd", "cd", "ae", "be", "ce", "de" });
+        }
+
+        [Test]
+        public void Test_5of4()
+        {
+            DoTestNofK("abcde", 4, new List<string>() { "abcd", "acde", "abde", "abce", "bcde" });
         }
     }
 }
