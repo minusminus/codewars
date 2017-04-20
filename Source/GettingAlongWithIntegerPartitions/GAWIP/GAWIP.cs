f875e45da460de280338e61ea59b9cb9f7983c8b
michal.smiechowski <michal.smiechowski@PC-MSM-W7>
2017-04-20 09:51:39 +0200

    getting along with integer partitions ...

--- Source/GettingAlongWithIntegerPartitions/GAWIP/GAWIP.cs
+++ Source/GettingAlongWithIntegerPartitions/GAWIP/GAWIP.cs
@@ -1,36 +1,31 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace GAWIP
 {
-    struct Result
-    {
-        public int Range;
-        public double Average;
-        public double Median;
-    }
-
     public class GAWIP
     {
         private BitArray _products;
         private int[] _idx;
-        private Result res;
 
         private void CalcNextItermediate(int n, int curr, int ix)
         {
+            //Console.WriteLine($"n={n}, curr={curr}, ix={ix}");
             if (curr == 1)
             {
+                //Console.WriteLine($"[{ix}]={_idx[ix - 1]}");
                 _products[_idx[ix - 1]] = true;
             }
             else
             {
                 _idx[ix] = _idx[ix - 1]*curr;
                 for(int j=n-curr; j>0; j--)
-                    CalcNextItermediate(n, j, ix + 1);
+                    CalcNextItermediate(curr, j, ix + 1);
             }
         }
 
@@ -45,32 +40,57 @@ namespace GAWIP
             }
         }
 
-        private void PrepareResult(int n)
+        private string PrepareResult(int n)
         {
             long max = 0;
             int cnt = 0;
-            _products.OfType<bool>().Select((b, i) =>
+            long sum = 0;
+            for (int i = 1; i < _products.Length; i++)
             {
-                if (b)
+                if (_products[i])
                 {
                     cnt++;
                     max = (i > max) ? i : max;
-                    Console.WriteLine($"{i}");
+                    sum += i;
+                    //Console.WriteLine($"{i}");
                 }
-                return 0;
-            });
+            }
+            Console.WriteLine($"cnt={cnt}, max={max}, sum={sum}");
+            int i1, i2;
+            if (cnt%2 == 1)
+                i1 = i2 = cnt/2 + 1;
+            else
+            {
+                i1 = cnt/2;
+                i2 = i1 + 1;
+            }
+            int j = 0;
+            int v1, v2;
+            for (int i = 1; (i < _products.Length) && (j>=Math.Max(i1,i2)); i++)
+            {
+                if (_products[i])
+                {
+                    j++;
+                    if (j == i1) v1 = i;
+                    if (j == i2) v2 = i;
+                }
+            }
+
+            var culture = CultureInfo.CreateSpecificCulture("en-US");
+            return $"Range: {max-1} Average: {((double)sum/(double)cnt).ToString("F2", culture)} Median: {((double)(i1+i2)/2.0).ToString("F2", culture)}";
         }
 
         public string Part(long n)
         {
-            _products = new BitArray((int) n*(int) n);
+            //if (n == 1) return "Range: 0 Average: 1.00 Median: 1.00";
+
+            //_products = new BitArray((int) n*(int) n);
+            _products = new BitArray(1000000);  //buffer size experimentally selected
             _idx = new int[n + 1];
             _idx[0] = 1;
 
             CalcProducts((int)n);
-            PrepareResult((int)n);
-
-            return "";
+            return PrepareResult((int)n);
         }
     }
 }
--- Source/GettingAlongWithIntegerPartitions/NGAWIP/NGAWIP.cs
+++ Source/GettingAlongWithIntegerPartitions/NGAWIP/NGAWIP.cs
@@ -12,10 +12,35 @@ namespace NGAWIP
         [Test]
         public void BasicKataTests()
         {
-            //_pobj.Part(2).ShouldBe("Range: 1 Average: 1.50 Median: 1.50");
-            //_pobj.Part(3).ShouldBe("Range: 2 Average: 2.00 Median: 2.00");
-            //_pobj.Part(4).ShouldBe("Range: 3 Average: 2.50 Median: 2.50");
+            _pobj.Part(2).ShouldBe("Range: 1 Average: 1.50 Median: 1.50");
+            _pobj.Part(3).ShouldBe("Range: 2 Average: 2.00 Median: 2.00");
+            _pobj.Part(4).ShouldBe("Range: 3 Average: 2.50 Median: 2.50");
             _pobj.Part(5).ShouldBe("Range: 5 Average: 3.50 Median: 3.50");
+        }
+
+        [Test]
+        public void Test1()
+        {
+            _pobj.Part(1).ShouldBe("Range: 0 Average: 1.00 Median: 1.00");
+        }
+
+        [Test]
+        public void Test_6_7()
+        {
+            _pobj.Part(6).ShouldBe("Range: 7 Average: 4.14 Median: 4.00");
+            _pobj.Part(7).ShouldBe("Range: 11 Average: 5.80 Median: 5.50");
+        }
+
+        [Test]
+        public void Test10()
+        {
+            _pobj.Part(10).ShouldBe("Range: 47 Average: 15.95 Median: 11.50");
+        }
+
+        [Test]
+        public void Test50()
+        {
+            _pobj.Part(50).ShouldBe("Range: 593711 Average: 35074.03 Median: 1628.50");
         }
     }
 }
