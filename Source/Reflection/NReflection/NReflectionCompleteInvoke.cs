a02f00d48f7487a1625f3e1a25f2aa47d0f21ea3
michal.smiechowski <michal.smiechowski@PC-MSM-W7>
2017-06-08 16:19:43 +0200

    reflection katas complete invoke ...

---
+++ Source/Reflection/NReflection/NReflectionCompleteInvoke.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+using Shouldly;
+using System.Linq;
+using System.Reflection;
+
+namespace NReflection
+{
+    public class testClassCI
+    {
+        public string returnvalue = "abcd";
+
+        public string testmethod()
+        {
+            return returnvalue;
+        }
+    }
+
+    public class NReflectionCompleteInvoke
+    {
+        Reflection.ReflectionCompleteInvoke _pobj = new Reflection.ReflectionCompleteInvoke();
+        testClassCI Helper = new testClassCI();
+
+        [Test]
+        public void NullTest()
+        {
+            _pobj.InvokeMethod(null).ShouldBeNull();
+        }
+
+        [Test]
+        public void EmptyTest()
+        {
+            _pobj.InvokeMethod("").ShouldBe("");
+        }
+
+        [Test]
+        public void UnknownTypeTest()
+        {
+            _pobj.InvokeMethod("unknownType").ShouldBeNull();
+        }
+
+        [Test]
+        public void SmallObjectTest()
+        {
+            _pobj.InvokeMethod("testClassCI").ShouldBe(Helper.returnvalue);
+        }
+    }
+}
---
+++ Source/Reflection/Reflection/ReflectionCompleteInvoke.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reflection
+{
+    public class ReflectionCompleteInvoke
+    {
+        public string InvokeMethod(string typeName)
+        {
+            if (String.IsNullOrEmpty(typeName)) return typeName;
+
+            //PMOMX,1d03e0e7, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null
+
+            Type t = null;
+            foreach (var assembly in AppDomain.CurrentDomain.GetAssemblies())
+            {
+                t = assembly.DefinedTypes.FirstOrDefault(x => x.Name == typeName);
+                if (t != null) break;
+            }
+            //if (t == null) Console.WriteLine("type null");
+            if (t == null) return null;
+
+            MethodInfo mi = t.GetMethods(BindingFlags.DeclaredOnly | BindingFlags.Instance | BindingFlags.Static |
+                                         BindingFlags.Public | BindingFlags.NonPublic)
+                .Where(x => x.ReturnType == typeof (string))
+                .FirstOrDefault(x => x.GetParameters().Count() == 0);
+            if (mi == null) return null;
+
+            var obj = Activator.CreateInstance(t);
+
+            return (string)mi.Invoke(obj, null);
+        }
+    }
+}
