0d5dcced9572687c0d62675832a41f3ae0358238
michal.smiechowski <michal.smiechowski@PC-MSM-W7>
2018-11-09 16:58:40 +0100

    added permutations generator

---
+++ Source/Skyscrapers NxN/NSkyscrapers/NPermutationGen.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+using Shouldly;
+using Skyscrapers;
+
+namespace NSkyscrapers
+{
+    [TestFixture]
+    public class NPermutationGen
+    {
+        private void CheckGeneratedList(List<int[]> check, List<int[]> expected)
+        {
+            check.Count.ShouldBe(expected.Count, "rozne dlugosci");
+            for (int i = 0; i < check.Count; i++)
+            {
+                check[i].Length.ShouldBe(expected[i].Length, $"rozne dlugosci tablic {i}");
+                for (int j = 0; j < check[i].Length; j++)
+                    check[i][j].ShouldBe(expected[i][j], $"niezgodne elementy {i},{j}");
+            }
+        }
+
+        [Test]
+        public void FourElemsPermTest()
+        {
+            PermutationGen obj = new PermutationGen();
+
+            int[] testdata = new int[4] {1, 2, 3, 4};
+            List<int[]> resList = new List<int[]>();
+            List<int[]> expectedList = new List<int[]>()
+            {
+                new int[]{1, 2, 3, 4}, new int[]{2, 1, 3, 4}, new int[]{1, 3, 2, 4}, new int[]{3, 1, 2, 4}, new int[]{2, 3, 1, 4}, new int[]{3, 2, 1, 4},
+                new int[]{1, 2, 4, 3}, new int[]{2, 1, 4, 3}, new int[]{1, 4, 2, 3}, new int[]{4, 1, 2, 3}, new int[]{2, 4, 1, 3}, new int[]{4, 2, 1, 3},
+                new int[]{1, 3, 4, 2}, new int[]{3, 1, 4, 2}, new int[]{1, 4, 3, 2}, new int[]{4, 1, 3, 2}, new int[]{3, 4, 1, 2}, new int[]{4, 3, 1, 2},
+                new int[]{2, 3, 4, 1}, new int[]{3, 2, 4, 1}, new int[]{2, 4, 3, 1}, new int[]{4, 2, 3, 1}, new int[]{3, 4, 2, 1}, new int[]{4, 3, 2, 1}
+            };
+
+            obj.Gen(testdata, resList);
+            CheckGeneratedList(resList, expectedList);
+        }
+    }
+}
---
+++ Source/Skyscrapers NxN/Skyscrapers/PermutationGen.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Skyscrapers
+{
+    /// <summary>
+    /// Generator permutacji podanej tablicy w kolejnosci antyleksykograficznej
+    /// Na podstawie "Algorytmika praktyczna" rozdział 4.1
+    /// </summary>
+    public class PermutationGen
+    {
+        private int[] _tbl;
+        private List<int[]> _resList;
+
+        private void SwapInTbl(int x, int y)
+        {
+            int t = _tbl[x];
+            _tbl[x] = _tbl[y];
+            _tbl[y] = t;
+        }
+
+        private void Perm(int m)
+        {
+            if (m == 0)
+            {
+                int[] item = new int[_tbl.Length];
+                Array.Copy(_tbl, 0, item, 0, _tbl.Length);
+                _resList.Add(item);
+            }
+            else
+            {
+                for (int i = 0; i <= m; i++)
+                {
+                    Perm(m - 1);
+                    if (i < m)
+                    {
+                        SwapInTbl(i, m);
+                        Array.Reverse(_tbl, 0, m);
+                    }
+                }
+            }
+        }
+
+        public void Gen(int[] tbl, List<int[]> resList)
+        {
+            _tbl = tbl;
+            _resList = resList;
+            Perm(_tbl.Length - 1);
+        }
+    }
+}
