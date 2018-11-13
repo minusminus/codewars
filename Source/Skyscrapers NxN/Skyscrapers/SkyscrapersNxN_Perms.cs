c98abed04e5eb7819846f0ea8a742c55db92148e
michal.smiechowski <michal.smiechowski@PC-MSM-W7>
2018-11-13 16:03:47 +0100

    skyscrapers permutation solver init

---
+++ Source/Skyscrapers NxN/Skyscrapers/SkyscrapersNxN_Perms.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Skyscrapers
+{
+    /// <summary>
+    /// rozwiazanie z wykorzystaniem list wszystkich permutacji N elementowego zbioru
+    /// </summary>
+    public class SkyscrapersNxN_Perms
+    {
+        private readonly int _n;
+        private readonly SkyscrapersPrecalcData _precalc;
+
+        public SkyscrapersNxN_Perms(int n, SkyscrapersPrecalcData precalc)
+        {
+            _n = n;
+            _precalc = precalc;
+        }
+
+        public int[][] Solve(int[] constraints)
+        {
+            return new int[_n][];
+        }
+
+    }
+}
