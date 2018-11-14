fe53ee3d0bddf4a84c383efab803d3d46078f9ee
michal.smiechowski <michal.smiechowski@PC-MSM-W7>
2018-11-14 12:16:46 +0100

    skyscrapers permutation solver...

--- Source/Skyscrapers NxN/Skyscrapers/SkyscrapersNxN_Perms.cs
+++ Source/Skyscrapers NxN/Skyscrapers/SkyscrapersNxN_Perms.cs
@@ -13,6 +13,7 @@ namespace Skyscrapers
     {
         private readonly int _n;
         private readonly SkyscrapersPrecalcData _precalc;
+        List<int[]>[] _lists;  //tablica list
 
         public SkyscrapersNxN_Perms(int n, SkyscrapersPrecalcData precalc)
         {
@@ -22,7 +23,57 @@ namespace Skyscrapers
 
         public int[][] Solve(int[] constraints)
         {
+            _lists = new List<int[]>[_n];  //tablica list zainicjalizowana nulami
+            for (int i = 0; i < _lists.Length; i++) _lists[i] = null;
+
+            //przetworzenie list dla wszystkich ograniczen
+            for (int i = 0; i < _n; i++)
+            {
+                if (constraints[i] == 0) continue;
+                AddList(i, constraints[i]);
+            }
+
+
+            _lists = null;
             return new int[_n][];
+        }
+
+        private void AddList(int index, int visibility)
+        {
+            List<int[]> l = _precalc.GetList(visibility);
+            _lists[index] = l;
+
+            //int removedcnt = l.RemoveAll(x => x[0] == 0);
+
+            //1. usunac z nowej listy wszystkie niepasujace do biezacych list
+            //2. usunac z biezacych to co nie pasuje do nowej listy
+            //3. usuwac z pionowych to co nie pasuje do poziomych i z poziomych to co nie pasuje do pionowych do momentu az nic nie zostanie usuniete
+        }
+
+        private bool[] GetAllItemsAtPosition(List<int[]> list, int pos )
+        {
+            bool[] res = new bool[_n];
+            for (int i = 0; i < _n; i++) res[i] = false;
+
+            foreach (int[] tbl in list)
+                res[tbl[pos] - 1] = true;
+
+            return res;
+        }
+
+        private void PrepareList(int index)
+        {
+            List<int[]> l = _lists[index];
+            int side = index/_n;
+            int sidepos = index%_n;
+
+            int startrange = 0;
+            if ((side & 1) == 0) startrange = _n;
+            for (int i = startrange; i < startrange + _n; i++)
+            {
+                if (_lists[i] == null) continue;
+                bool[] toremove = GetAllItemsAtPosition(_lists[i], 0);
+            }
         }
 
     }
