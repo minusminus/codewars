888c5bceda8ad62133556fc73bd1ca1d163fc4ba
minusminus <minusminus@tlen.pl>
2018-11-30 21:08:30 +0100

    refactored list reduction
    not working on 6x6 tests

--- Source/Skyscrapers NxN/Skyscrapers/SkyscrapersNxN_Perms.cs
+++ Source/Skyscrapers NxN/Skyscrapers/SkyscrapersNxN_Perms.cs
@@ -23,7 +23,7 @@ namespace Skyscrapers
 
         public int[][] Solve(int[] constraints)
         {
-            _lists = new List<int[]>[4*_n];  //tablica list zainicjalizowana nulami
+            _lists = new List<int[]>[4 * _n];  //tablica list zainicjalizowana nulami
 
             //zainicjalizowanie list dla wszystkich ograniczen
             for (int i = 0; i < _lists.Length; i++)
@@ -31,41 +31,11 @@ namespace Skyscrapers
                     _lists[i] = _precalc.GetList(constraints[i]);
 
             //redukcja w poziomie i pionie wszystkich list
-            int deleted = 1;
-            while (deleted > 0)
-            {
-                deleted = ReduceHorizontal();
-                deleted += ReduceVertical();
-            }
+            while (ReduceLists() > 0) ;
 
             //wygenerowanie wynikowej tabeli
             SkyscraperData_Perms data = new SkyscraperData_Perms(_n);
-            for (int i = 0; i < _lists.Length; i++)
-                if (_lists[i] != null)
-                    for (int j = 0; j < _n; j++)
-                        data.SetSingleElement(GetDataRowCol(i, j), _lists[i][0][j]);
-            //_lists = null;
-
-            List<Tuple<int, int>> notSet = new List<Tuple<int, int>>();
-            for (int row = 0; row < _n; row++)
-                for (int col = 0; col < _n; col++)
-                    if (data.Data[row, col] == 0)
-                        notSet.Add(new Tuple<int, int>(row, col));
-
-            while (notSet.Count > 0)
-            {
-                int p = 0;
-                while (p < notSet.Count)
-                {
-                    int x = (data.SetInRow[notSet[p].Item1] | data.SetInCol[notSet[p].Item2]) ^ SkyscraperData_Perms.InitialValues[_n];
-                    if (data.CountBits(x) == 1)
-                    {
-                        data.SetSingleElementMask(notSet[p].Item1, notSet[p].Item2, x);
-                        notSet.RemoveAt(p);
-                    }
-                    else p++;
-                }
-            }
+            PopulateResultsData(data);
 
             int[][] res = new int[_n][];
             for (int i = 0; i < _n; i++)
@@ -75,10 +45,8 @@ namespace Skyscrapers
                     res[i][j] = Array.IndexOf(SkyscraperData.Masks, data.Data[i, j]);
             }
 
-            //data = null;
             return res;
         }
-
 
         private List<int>[] GetAllowedItems(List<int[]> list)
         {
@@ -111,52 +79,40 @@ namespace Skyscrapers
             return false;
         }
 
-        private int ReduceHorizontal()
+        private List<int>[][] CreateAllowedItemsList(int nPart)
         {
-            //redukcja list poziomych (z prawej i lewej strony)
-
-            //dla list gornych wyznaczamy macierz dopuszczalnych elementow na kazdej pozycji
-            //dla dolnych analogicznie
-            List<int>[][] allowedTop = new List<int>[_n][];
-            List<int>[][] allowedBottom = new List<int>[_n][];
+            List<int>[][] res = new List<int>[_n][];
             for (int i = 0; i < _n; i++)
-                if (_lists[i] != null)
-                    allowedTop[i] = GetAllowedItems(_lists[i]);
-            for (int i = 0; i < _n; i++)
-                if (_lists[2 * _n + i] != null)
-                    allowedBottom[i] = GetAllowedItems(_lists[2 * _n + i]);
+                if (_lists[nPart * _n + i] != null)
+                    res[i] = GetAllowedItems(_lists[nPart * _n + i]);
+            return res;
+        }
 
-            //potem filtrujemy listy boczne za pomoca tych macierzy
+        private int ReduceListsDir(int nPart, List<int>[][] allowedTop, List<int>[][] allowedBottom)
+        {
             int deleted = 0;
             for (int i = 0; i < _n; i++)
-                if (_lists[_n + i] != null)
-                    deleted += _lists[_n + i].RemoveAll(tbl => CheckIfPermToRemove(tbl, i, allowedTop, allowedBottom));
-            for (int i = 0; i < _n; i++)
-                if (_lists[3 * _n + i] != null)
-                    deleted += _lists[3 * _n + i].RemoveAll(tbl => CheckIfPermToRemove(tbl, i, allowedBottom, allowedTop));
+                if (_lists[nPart * _n + i] != null)
+                    deleted += _lists[nPart * _n + i].RemoveAll(tbl => CheckIfPermToRemove(tbl, i, allowedTop, allowedBottom));
             return deleted;
         }
 
-        private int ReduceVertical()
+        private int ReduceLists()
         {
-            //redukcja list pionowych (gornej i dolnej)
+            //reduce horizontal
+            List<int>[][] allowedTop = CreateAllowedItemsList(0);   //top
+            List<int>[][] allowedBottom = CreateAllowedItemsList(2);    //bottom
 
-            List<int>[][] allowedRight = new List<int>[_n][];
-            List<int>[][] allowedLeft = new List<int>[_n][];
-            for (int i = 0; i < _n; i++)
-                if (_lists[_n + i] != null)
-                    allowedRight[i] = GetAllowedItems(_lists[_n + i]);
-            for (int i = 0; i < _n; i++)
-                if (_lists[3 * _n + i] != null)
-                    allowedLeft[i] = GetAllowedItems(_lists[3 * _n + i]);
+            int deleted = ReduceListsDir(1, allowedTop, allowedBottom);
+            deleted += ReduceListsDir(3, allowedBottom, allowedTop);
 
-            int deleted = 0;
-            for (int i = 0; i < _n; i++)
-                if (_lists[i] != null)
-                    deleted += _lists[i].RemoveAll(tbl => CheckIfPermToRemove(tbl, i, allowedLeft, allowedRight));
-            for (int i = 0; i < _n; i++)
-                if (_lists[2 * _n + i] != null)
-                    deleted += _lists[2 * _n + i].RemoveAll(tbl => CheckIfPermToRemove(tbl, i, allowedRight, allowedLeft));
+            //reduce vertical
+            allowedTop = CreateAllowedItemsList(1); //right
+            allowedBottom = CreateAllowedItemsList(3);  //left
+
+            deleted += ReduceListsDir(0, allowedBottom, allowedTop);
+            deleted += ReduceListsDir(2, allowedTop, allowedBottom);
+
             return deleted;
         }
 
@@ -187,5 +143,36 @@ namespace Skyscrapers
             return new Tuple<int, int>(row, col);
         }
 
+        private void PopulateResultsData(SkyscraperData_Perms data)
+        {
+            //ustawienie elementow ze zredukowanych list
+            for (int i = 0; i < _lists.Length; i++)
+                if (_lists[i] != null)
+                    for (int j = 0; j < _n; j++)
+                        data.SetSingleElement(GetDataRowCol(i, j), _lists[i][0][j]);
+
+            //lista nieustawionych elementow
+            List<Tuple<int, int>> notSet = new List<Tuple<int, int>>();
+            for (int row = 0; row < _n; row++)
+                for (int col = 0; col < _n; col++)
+                    if (data.Data[row, col] == 0)
+                        notSet.Add(new Tuple<int, int>(row, col));
+
+            //ustawienie brakujacych elementow
+            while (notSet.Count > 0)
+            {
+                int p = 0;
+                while (p < notSet.Count)
+                {
+                    int x = (data.SetInRow[notSet[p].Item1] | data.SetInCol[notSet[p].Item2]) ^ SkyscraperData_Perms.InitialValues[_n];
+                    if (data.CountBits(x) == 1)
+                    {
+                        data.SetSingleElementMask(notSet[p].Item1, notSet[p].Item2, x);
+                        notSet.RemoveAt(p);
+                    }
+                    else p++;
+                }
+            }
+        }
     }
 }
