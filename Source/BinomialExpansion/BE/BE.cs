3fdbe6faed1806f6d3d00fb5ec075f50bdbd54ba
Michał Śmiechowski <Michal.Smiechowski@DSA.Polska>
2018-12-27 17:04:42 +0100

    binomial expansion start

---
+++ Source/BinomialExpansion/BE/BE.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BE
+{
+    public class BE
+    {
+        public string Expand(string expr)
+        {
+            return "";
+        }
+    }
+}
---
+++ Source/BinomialExpansion/NBE/NBE.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using NUnit.Framework;
+using Shouldly;
+
+namespace NBE
+{
+    [TestFixture]
+    public class NBE
+    {
+        private readonly BE.BE _testObj = new BE.BE();
+
+        [Test]
+        public void TestBPositive()
+        {
+            _testObj.Expand("(x+1)^0").ShouldBe("1");
+            _testObj.Expand("(x+1)^1").ShouldBe("x+1");
+            _testObj.Expand("(x+1)^2").ShouldBe("x^2+2x+1");
+        }
+
+        [Test]
+        public void TestBNegative()
+        {
+            _testObj.Expand("(x-1)^0").ShouldBe("1");
+            _testObj.Expand("(x-1)^1").ShouldBe("x-1");
+            _testObj.Expand("(x-1)^2").ShouldBe("x^2-2x+1");
+        }
+
+        [Test]
+        public void TestAPositive()
+        {
+            _testObj.Expand("(5m+3)^4").ShouldBe("625m^4+1500m^3+1350m^2+540m+81");
+            _testObj.Expand("(2x-3)^3").ShouldBe("8x^3-36x^2+54x-27");
+            _testObj.Expand("(7x-7)^0").ShouldBe("1");
+        }
+
+        [Test]
+        public void TestANegative()
+        {
+            _testObj.Expand("(-5m+3)^4").ShouldBe("625m^4-1500m^3+1350m^2-540m+81");
+            _testObj.Expand("(-2k-3)^3").ShouldBe("-8k^3-36k^2-54k-27");
+            _testObj.Expand("(-7x-7)^0").ShouldBe("1");
+        }
+    }
+}
