f38cecf3e4791ffa4abe90a9ade9f2f0bf8bafbc
Michał Śmiechowski <Michal.Smiechowski@DSA.Polska>
2018-12-31 12:51:58 +0100

    square into squares initialization

---
+++ Source/SquareIntoSquares/NSqIntoSq/NSIS.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using NUnit.Framework;
+using Shouldly;
+using SqIntoSq;
+
+namespace NSqIntoSq
+{
+    [TestFixture]
+    public class NSIS
+    {
+        private readonly SIS _testObj = new SIS();
+
+        [Test]
+        public void TestSampleKata()
+        {
+            _testObj.Decompose(11).ShouldBe("1 2 4 10");
+        }
+    }
+}
---
+++ Source/SquareIntoSquares/SqIntoSq/SIS.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SqIntoSq
+{
+    /// <summary>
+    /// 
+    /// suma kwadratow n pierwszych liczb naturalnych: n(n+1)(2n+1)/6
+    /// </summary>
+    public class SIS
+    {
+        private readonly Stack<long> _solution = new Stack<long>();
+
+        public bool Process(long n, long spaceleft)
+        {
+            if (spaceleft == 0) return true;
+            for (long i = n; i*(i+1)*(2*i+1)/6 >= spaceleft; i--)
+            {
+                _solution.Push(i);
+                long nextn = (long)Math.Floor(Math.Sqrt(spaceleft - i*i));
+                if (Process(nextn, spaceleft - nextn*nextn)) return true;
+                _solution.Pop();
+            }
+            return false;
+        }
+
+        public string PrepareResult()
+        {
+            long[] arr = _solution.ToArray();
+            Array.Reverse(arr);
+            return string.Join(" ", arr);
+        }
+
+        public string Decompose(long n)
+        {
+            _solution.Clear();
+            if (Process(n - 1, n*n))
+                return PrepareResult();
+            return "";
+        }
+    }
+}
