9d72a971a1f87e198238c325dcfb69b16750ebfe
Michał Śmiechowski <Michal.Smiechowski@DSA.Polska>
2018-12-31 13:05:32 +0100

    square into squares - more tests added

--- Source/SquareIntoSquares/NSqIntoSq/NSIS.cs
+++ Source/SquareIntoSquares/NSqIntoSq/NSIS.cs
@@ -15,5 +15,24 @@ namespace NSqIntoSq
         {
             _testObj.Decompose(11).ShouldBe("1 2 4 10");
         }
+
+        [Test]
+        public void Test2()
+        {
+            _testObj.Decompose(50).ShouldBe("1 3 5 8 49");
+        }
+
+        [Test]
+        public void TestsFromDiscourse()
+        {
+            _testObj.Decompose(18351).ShouldBe("1, 3, 4, 5, 13, 191, 18350");
+            _testObj.Decompose(38477).ShouldBe("1, 2, 3, 4, 5, 13, 277, 38476");
+        }
+
+        [Test]
+        public void TestsFromDiscourse_LargeNGap()
+        {
+            _testObj.Decompose(100000).ShouldBe("1 2 4 13 447 99999");
+        }
     }
 }
