8b371eb3c694368aba6f371fe4e8ae3a477b9a0c
Michał Śmiechowski <Michal.Smiechowski@dsa.com.pl>
2019-01-14 10:28:36 +0100

    DecodeTheMorseCodeForReal - init

---
+++ Source/DecodeTheMorseCodeForReal/DecodeTheMorseCodeForReal/DTMCFR.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DecodeTheMorseCodeForReal
+{
+    public class DTMCFR
+    {
+        public string decodeBitsAdvanced(string bits)
+        {
+            return "";
+        }
+
+        public string decodeMorse(string morseCode)
+        {
+            // Map morse code using map Preloaded.MORSE_CODE
+            return "";
+        }
+    }
+}
---
+++ Source/DecodeTheMorseCodeForReal/NDecodeTheMorseCodeForReal/NDTMCFR.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using DecodeTheMorseCodeForReal;
+using NUnit.Framework;
+using Shouldly;
+
+namespace NDecodeTheMorseCodeForReal
+{
+    [TestFixture]
+    public class NDTMCFR
+    {
+        private readonly DTMCFR _testObj = new DTMCFR();
+
+        [Test]
+        public void BasicKataTest()
+        {
+            _testObj.decodeMorse(
+                _testObj.decodeBitsAdvanced(
+                    "0000000011011010011100000110000001111110100111110011111100000000000111011111111011111011111000000101100011111100000111110011101100000100000"))
+                .ShouldBe("HEY JUDE");
+        }
+    }
+}
