ad01414dd85c69aea3e58d45205716e644b40fd6
Michał Śmiechowski <Michal.Smiechowski@dsa.com.pl>
2019-01-14 12:56:39 +0100

    DecodeTheMorseCodeForReal - grouping input bits

--- Source/DecodeTheMorseCodeForReal/DecodeTheMorseCodeForReal/DTMCFR.cs
+++ Source/DecodeTheMorseCodeForReal/DecodeTheMorseCodeForReal/DTMCFR.cs
@@ -8,6 +8,22 @@ namespace DecodeTheMorseCodeForReal
 {
     public class DTMCFR
     {
+        public List<DTMCFRDataChunk> ChunkBits(string bits)
+        {
+            List<DTMCFRDataChunk> res = new List<DTMCFRDataChunk>();
+
+            foreach (char c in bits)
+            {
+                if ((res.Count == 0) || (res.Last().Symbol != c))
+                    res.Add(new DTMCFRDataChunk() {Symbol = c, Length = 0});
+                res.Last().Length++;
+            }
+            if (res.First().Symbol == '0') res.RemoveAt(0);
+            if (res.Last().Symbol == '0') res.RemoveAt(res.Count - 1);
+
+            return res;
+        }
+
         public string decodeBitsAdvanced(string bits)
         {
             return "";
---
+++ Source/DecodeTheMorseCodeForReal/NDecodeTheMorseCodeForReal/NDTMCFRInternalTests.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DecodeTheMorseCodeForReal;
+using NUnit.Framework;
+using Shouldly;
+
+namespace NDecodeTheMorseCodeForReal
+{
+    [TestFixture]
+    public class NDTMCFRInternalTests
+    {
+        private readonly DTMCFR _testObj = new DTMCFR();
+
+        private void CheckChunkBits(string bits, List<DTMCFRDataChunk> expected)
+        {
+            List<DTMCFRDataChunk> data = _testObj.ChunkBits(bits);
+            data.Count.ShouldBe(expected.Count);
+            for (int i = 0; i < data.Count; i++)
+            {
+                data[i].Symbol.ShouldBe(expected[i].Symbol, $"{i}");
+                data[i].Length.ShouldBe(expected[i].Length, $"{i}");
+            }
+        }
+
+        [Test]
+        public void ChunkTests()
+        {
+            CheckChunkBits("0000000011011010011100000100000",
+                new List<DTMCFRDataChunk>()
+                {
+                    new DTMCFRDataChunk() {Symbol = '1', Length = 2},
+                    new DTMCFRDataChunk() {Symbol = '0', Length = 1},
+                    new DTMCFRDataChunk() {Symbol = '1', Length = 2},
+                    new DTMCFRDataChunk() {Symbol = '0', Length = 1},
+                    new DTMCFRDataChunk() {Symbol = '1', Length = 1},
+                    new DTMCFRDataChunk() {Symbol = '0', Length = 2},
+                    new DTMCFRDataChunk() {Symbol = '1', Length = 3},
+                    new DTMCFRDataChunk() {Symbol = '0', Length = 5},
+                    new DTMCFRDataChunk() {Symbol = '1', Length = 1},
+                });
+
+            CheckChunkBits("00000000110110100111000001",
+                new List<DTMCFRDataChunk>()
+                {
+                    new DTMCFRDataChunk() {Symbol = '1', Length = 2},
+                    new DTMCFRDataChunk() {Symbol = '0', Length = 1},
+                    new DTMCFRDataChunk() {Symbol = '1', Length = 2},
+                    new DTMCFRDataChunk() {Symbol = '0', Length = 1},
+                    new DTMCFRDataChunk() {Symbol = '1', Length = 1},
+                    new DTMCFRDataChunk() {Symbol = '0', Length = 2},
+                    new DTMCFRDataChunk() {Symbol = '1', Length = 3},
+                    new DTMCFRDataChunk() {Symbol = '0', Length = 5},
+                    new DTMCFRDataChunk() {Symbol = '1', Length = 1},
+                });
+
+            CheckChunkBits("11011010011100000100000",
+                new List<DTMCFRDataChunk>()
+                {
+                    new DTMCFRDataChunk() {Symbol = '1', Length = 2},
+                    new DTMCFRDataChunk() {Symbol = '0', Length = 1},
+                    new DTMCFRDataChunk() {Symbol = '1', Length = 2},
+                    new DTMCFRDataChunk() {Symbol = '0', Length = 1},
+                    new DTMCFRDataChunk() {Symbol = '1', Length = 1},
+                    new DTMCFRDataChunk() {Symbol = '0', Length = 2},
+                    new DTMCFRDataChunk() {Symbol = '1', Length = 3},
+                    new DTMCFRDataChunk() {Symbol = '0', Length = 5},
+                    new DTMCFRDataChunk() {Symbol = '1', Length = 1},
+                });
+        }
+    }
+}
