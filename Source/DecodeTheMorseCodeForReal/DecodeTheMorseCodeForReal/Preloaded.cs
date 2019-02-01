2e68e9c749cadff63a0b5aa593f91e0c55445511
Michał Śmiechowski <Michal.Smiechowski@dsa.com.pl>
2019-02-01 16:04:25 +0100

    DecodeTheMorseCodeForReal - testing for single word...

---
+++ Source/DecodeTheMorseCodeForReal/DecodeTheMorseCodeForReal/Preloaded.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DecodeTheMorseCodeForReal
+{
+    class Preloaded
+    {
+        public static readonly Dictionary<string, char> MORSE_CODE = new Dictionary<string, char>
+        {
+            {".-", 'A'},
+            {"-...", 'B'},
+            {"-.-.", 'C'},
+            {"-..", 'D'},
+            {".", 'E'},
+            {"..-.", 'F'},
+            {"--.", 'G'},
+            {"....", 'H'},
+            {"..", 'I'},
+            {".---", 'J'},
+            {"-.-", 'K'},
+            {".-..", 'L'},
+            {"--", 'M'},
+            {"-.", 'N'},
+            {"---", 'O'},
+            {".--.", 'P'},
+            {"--.-", 'Q'},
+            {".-.", 'R'},
+            {"...", 'S'},
+            {"-", 'T'},
+            {"..-", 'U'},
+            {"...-", 'V'},
+            {".--", 'W'},
+            {"-..-", 'X'},
+            {"-.--", 'Y'},
+            {"--..", 'Z'},
+            {"-----", '0'},
+            {".----", '1'},
+            {"..---", '2'},
+            {"...--", '3'},
+            {"....-", '4'},
+            {".....", '5'},
+            {"-....", '6'},
+            {"--...", '7'},
+            {"---..", '8'},
+            {"----.", '9'}
+        };
+    }
+}
--- Source/DecodeTheMorseCodeForReal/NDecodeTheMorseCodeForReal/NDTMCFR.cs
+++ Source/DecodeTheMorseCodeForReal/NDecodeTheMorseCodeForReal/NDTMCFR.cs
@@ -18,5 +18,14 @@ namespace NDecodeTheMorseCodeForReal
                     "0000000011011010011100000110000001111110100111110011111100000000000111011111111011111011111000000101100011111100000111110011101100000100000"))
                 .ShouldBe("HEY JUDE");
         }
+
+        [Test]
+        public void SingleWord()
+        {
+            _testObj.decodeMorse(
+                _testObj.decodeBitsAdvanced(
+                    "000000001101101001110000011000000111111010011111001111110000000000"))
+                .ShouldBe("HEY");
+        }
     }
 }
