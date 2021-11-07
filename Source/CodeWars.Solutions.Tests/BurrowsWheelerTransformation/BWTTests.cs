92ab63bdc2e5e9fd0e353767b0b065cd546146b9
minusminus <minusminus@tlen.pl>
2021-11-07 22:21:59 +0100

    - added Burrows-Wheeler-Transformation encoding

---
+++ Source/CodeWars.Solutions.Tests/BurrowsWheelerTransformation/BWTTests.cs
@@ -0,0 +1,74 @@
+﻿using NUnit.Framework;
+using Shouldly;
+using CodeWars.Solutions.BurrowsWheelerTransformation;
+using System.Linq;
+using System;
+using System.Collections.Generic;
+
+namespace CodeWars.Solutions.Tests.BurrowsWheelerTransformation
+{
+    [TestFixture]
+    public class BWTTests
+    {
+        [TestCase("banana", "bnnaaa", 3)]
+        [TestCase("banaca", "nbcaaa", 3)]
+        [TestCase("banac", "nbcaa", 2)]
+        [TestCase("ban ana", "nb naaa", 4)]
+        [TestCase("bAnanA", "bnnAaA", 3)]
+        [TestCase("bananabar", "nnbbraaaa", 4)]
+        [TestCase("Humble Bundle", "e emnllbduuHB", 2)]
+        [TestCase("Mellow Yellow", "ww MYeelllloo", 1)]
+        public void Encode__EncodesCorrectly(string input, string expectedEncoded, int expectedIndex)
+        {
+            var result = BWT.Encode(input);
+            result.Item1.ShouldBe(expectedEncoded);
+            result.Item2.ShouldBe(expectedIndex);
+        }
+
+        [TestCase("bnnaaa", 3, "banana")]
+        [TestCase("nbcaaa", 3, "banaca")]
+        [TestCase("nbcaa", 2, "banac")]
+        [TestCase("nnbbraaaa", 4, "bananabar")]
+        [TestCase("e emnllbduuHB", 2, "Humble Bundle")]
+        [TestCase("ww MYeelllloo", 1, "Mellow Yellow")]
+        [TestCase("xxxxxxxxxx", 0, "xxxxxxxxxx")]
+        public void Decode__DecodesCorrectly(string encoded, int index, string expected)
+        {
+            BWT.Decode(encoded, index).ShouldBe(expected);
+        }
+
+        [TestCase("banana")]
+        [TestCase("Humble Bundle")]
+        [TestCase("Mellow Yellow")]
+        public void CustomTest(string input)
+        {
+            var rotations = Enumerable
+                .Range(0, input.Length)
+                .Select(i => input.Substring(input.Length - i, i) + input.Substring(0, input.Length - i))
+                .ToArray();
+            Console.WriteLine("-= rotations:");
+            foreach (var item in rotations)
+                Console.WriteLine(item);
+
+            Console.WriteLine("-= sorted:");
+            foreach (var item in rotations.OrderBy(x => x, StringComparer.Ordinal))
+                Console.WriteLine(item);
+
+            Console.WriteLine("-= custom sorted:");
+            foreach (var item in rotations.OrderBy(x => x, new CustomStringComparer()))
+                Console.WriteLine(item);
+        }
+
+        private class CustomStringComparer : IComparer<string>
+        {
+            public int Compare(string x, string y)
+            {
+                if (x == y) return 0;
+                if (x.StartsWith(y)) return 1;
+                if (y.StartsWith(x)) return -1;
+
+                return StringComparer.Ordinal.Compare(x, y);
+            }
+        }
+    }
+}
---
+++ Source/CodeWars.Solutions/BurrowsWheelerTransformation/BWT.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars.Solutions.BurrowsWheelerTransformation
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public static class BWT
+    {
+        private class BWTStringComparer : IComparer<string>
+        {
+            public int Compare(string x, string y)
+            {
+                if (x == y) return 0;
+                if (x.StartsWith(y)) return -1;
+                if (y.StartsWith(x)) return 1;
+
+                return StringComparer.Ordinal.Compare(x, y);
+            }
+        }
+
+        public static Tuple<string, int> Encode(string s) => 
+            GetSuffixes(s)
+                .SortSuffixes()
+                .GetLastColumnAndOriginalTextIndex(s);
+
+        private static IEnumerable<string> GetSuffixes(string s) => 
+            Enumerable
+                .Range(0, s.Length)
+                .Select(i => s.Substring(i, s.Length - i));
+
+        private static IEnumerable<string> SortSuffixes(this IEnumerable<string> suffixes) =>
+            suffixes
+                .OrderBy(x => x, new BWTStringComparer());
+
+        private static Tuple<string, int> GetLastColumnAndOriginalTextIndex(this IEnumerable<string> sortedSuffixes, string originalString)
+        {
+            int foundIndex = -1;
+
+            char CheckIndexAndGetLastCharInRow(string suffix, int currentIndex)
+            {
+                if (suffix.Length == originalString.Length) foundIndex = currentIndex;
+                return originalString[(originalString.Length - suffix.Length - 1 + originalString.Length) % originalString.Length];
+            }
+
+            return new Tuple<string, int>(
+                string.Join("", sortedSuffixes.Select((suffix, i) => CheckIndexAndGetLastCharInRow(suffix, i))), 
+                foundIndex);
+        }
+
+        public static string Decode(string s, int i)
+        {
+            return string.Empty;
+        }
+    }
+}
