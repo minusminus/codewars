b059000d05b8a9cfb3a6ac11c12415f9c99c095b
minusminus <minusminus@tlen.pl>
2021-11-10 22:41:38 +0100

    - tests of random strings generators
    - added kata description

--- Problems/CodeWars.Katas.Tests/HeroSuffixQuest/HeroTests.cs
+++ Problems/CodeWars.Katas.Tests/HeroSuffixQuest/HeroTests.cs
@@ -1,8 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using CodeWars.Katas.HeroSuffixQuest;
 using NUnit.Framework;
 
@@ -11,8 +8,18 @@ namespace CodeWars.Katas.Tests.HeroSuffixQuest
     [TestFixture]
     public class HeroTests
     {
+        private const int MaxTextLength = 100000;
+
         private readonly Random _random = new Random();
         private readonly char[] _randomizerAlphabet = "abcdefghijklmnopqrstuvwxyz0123456789".ToCharArray();
+        private readonly char[] _preinitializedRandomChars = new char[MaxTextLength];
+
+        [OneTimeSetUp]
+        public void OneTimeSetUp()
+        {
+            for (int i = 0; i < _preinitializedRandomChars.Length; i++)
+                _preinitializedRandomChars[i] = _randomizerAlphabet[_random.Next(_randomizerAlphabet.Length)];
+        }
 
         [TestCase("", 1, "")]
         [TestCase("abcd", -1, "")]
@@ -27,30 +34,59 @@ namespace CodeWars.Katas.Tests.HeroSuffixQuest
         [TestCase("dcba", 2, "cba")]
         [TestCase("dcba", 3, "dcba")]
         [TestCase("dcba", 4, "")]
-        [Order(1)]  //these test should run first
+        [Order(1)]  //these tests should be run first
         public void FindPassword__ReturnsCorrectly(string engravedText, int numberOnTheDoor, string expected)
         {
             Assert.AreEqual(expected, Hero.FindPassword(engravedText, numberOnTheDoor));
         }
 
-        [TestCase(10, 1000)]    //17 ms
-        [TestCase(10, 10000)]   //215 ns
-        [TestCase(10, 20000)]   //480 ms
-        [TestCase(10, 30000)]   //750 ms
-        [TestCase(10, 40000)]   //1 sec
-        //[TestCase(10, 50000)]   //1.3 sec
-        //[TestCase(10, 100000)]  //2.8 sec
-        //[TestCase(10, 1000000)]   //40 sec
-        [TestCase(100, 1000)]   //155 ms
-        [TestCase(100, 2000)]   //340 ms
-        [TestCase(100, 3000)]   //530 ms
-        [TestCase(100, 4000)]   //740 ms
-        [TestCase(100, 5000)]   //940 ms
-        //[TestCase(100, 10000)]  //2.2 sec
-        //[TestCase(100, 100000)] //28 sec
-        public void AreYouFasterThanWater(int numberOfDoors, int textLength)
+        [TestCase(1000)]    //17 ms
+        [TestCase(10000)]   //205 ns
+        [TestCase(20000)]   //450 ms
+        [TestCase(30000)]   //705 ms
+        [TestCase(40000)]   //980 ms
+        //[TestCase(50000)]   //1.3 sec
+        //[TestCase(100000)]  //2.8 sec
+        //[TestCase(1000000)]   //40 sec
+        public void AreYouFasterThanWater_10Doors(int textLength)
         {
-            for(int door = 0; door<numberOfDoors; door++)
+            TestWithRandomString(10, textLength);
+        }
+
+        [TestCase(1000)]   //155 ms
+        [TestCase(2000)]   //330 ms
+        [TestCase(3000)]   //520 ms
+        [TestCase(4000)]   //710 ms
+        [TestCase(5000)]   //925 ms
+        //[TestCase(10000)]  //2.2 sec
+        //[TestCase(100000)] //28 sec
+        public void AreYouFasterThanWater_100Doors(int textLength)
+        {
+            TestWithRandomString(100, textLength);
+        }
+
+        [Test]
+        public void GetRandomStringTest([Values(10, 100)] int iterations, [Values(1000, 10000, 40000)] int textLength)
+        {
+            List<string> tmp = new List<string>();
+            for (int i = 0; i < iterations; i++)
+                tmp.Add(GetRandomString(textLength));
+            Assert.IsNotEmpty(tmp);
+        }
+
+        [Test]
+        public void GetRandomStringPreinitializedTest([Values(10, 100)] int iterations, [Values(1000, 10000, 40000)] int textLength)
+        {
+            List<string> tmp = new List<string>();
+            for (int i = 0; i < iterations; i++)
+                tmp.Add(GetRandomStringPreinitialized(textLength));
+            Assert.IsNotEmpty(tmp);
+        }
+
+        private void TestWithRandomString(int numberOfDoors, int textLength)
+        {
+            Assert.LessOrEqual(textLength, MaxTextLength);
+            for (int door = 0; door < numberOfDoors; door++)
             {
                 string engravedText = GetRandomString(textLength);
                 int numberOnTheDoor = GetRandomNumberOnTheDoor(textLength);
@@ -64,6 +100,15 @@ namespace CodeWars.Katas.Tests.HeroSuffixQuest
             char[] stringChars = new char[length];
             for (int i = 0; i < length; i++)
                 stringChars[i] = _randomizerAlphabet[_random.Next(_randomizerAlphabet.Length)];
+            return new string(stringChars);
+        }
+
+        private string GetRandomStringPreinitialized(int length)
+        {
+            int randomStart = _random.Next(_preinitializedRandomChars.Length - length);
+            char[] stringChars = new char[length];
+            for (int i = 0; i < length; i++)
+                stringChars[i] = _preinitializedRandomChars[randomStart + i];
             return new string(stringChars);
         }
 
