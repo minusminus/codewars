5581c292e45bfdf86bdbb242952c0839c57723ea
minusminus <minusminus@tlen.pl>
2021-11-13 22:24:08 +0100

    - added first version of Become Immortal

---
+++ Source/CodeWars.Solutions.Tests/BecomeImmortal/BecomeImmortalTests.cs
@@ -0,0 +1,62 @@
+﻿using NUnit.Framework;
+using Shouldly;
+using CodeWars.Solutions.BecomeImmortal;
+
+namespace CodeWars.Solutions.Tests.BecomeImmortal
+{
+    [TestFixture]
+    public class BecomeImmortalTests
+    {
+        const uint NoT = 1000000000u;
+
+        [TestCase(2u, 2u, 0u, NoT, 2u)]
+        [TestCase(4u, 4u, 0u, NoT, 24u)]
+        [TestCase(8u, 8u, 0u, NoT, 224u)]
+        public void Find_FullSquaresOnly__ReturnsCorrectly(uint m, uint n, uint l, uint t, uint expected)
+        {
+            CodeWars.Solutions.BecomeImmortal.BecomeImmortal.Find(m, n, l, t).ShouldBe(expected);
+        }
+
+        [TestCase(2u, 2u, 1u, NoT, 0u)]
+        [TestCase(4u, 4u, 1u, NoT, 12u)]
+        [TestCase(8u, 8u, 1u, NoT, 168u)]
+        [TestCase(2u, 2u, 2u, NoT, 0u)]
+        [TestCase(4u, 4u, 2u, NoT, 0u)]
+        [TestCase(8u, 8u, 2u, NoT, 120u)]
+        public void Find_FullSquares_WithL__ReturnsCorrectly(uint m, uint n, uint l, uint t, uint expected)
+        {
+            CodeWars.Solutions.BecomeImmortal.BecomeImmortal.Find(m, n, l, t).ShouldBe(expected);
+        }
+
+        [TestCase(10u, 8u, 0u, NoT, 224u + 184u)]
+        [TestCase(15u, 8u, 0u, NoT, 224u + 644u)]
+        [TestCase(10u, 8u, 1u, NoT, 168u + 168u)]
+        [TestCase(15u, 8u, 1u, NoT, 168u + 588u)]
+        public void Find_WithPartOnRight__ReturnsCorrectly(uint m, uint n, uint l, uint t, uint expected)
+        {
+            CodeWars.Solutions.BecomeImmortal.BecomeImmortal.Find(m, n, l, t).ShouldBe(expected);
+        }
+
+        [TestCase(8u, 10u, 0u, NoT, 224u + 184u)]
+        [TestCase(8u, 15u, 0u, NoT, 224u + 644u)]
+        [TestCase(8u, 10u, 1u, NoT, 168u + 168u)]
+        [TestCase(8u, 15u, 1u, NoT, 168u + 588u)]
+        public void Find_WithPartBelow__ReturnsCorrectly(uint m, uint n, uint l, uint t, uint expected)
+        {
+            CodeWars.Solutions.BecomeImmortal.BecomeImmortal.Find(m, n, l, t).ShouldBe(expected);
+        }
+
+        [TestCase(10u, 10u, 0u, NoT, 224u + 184u + 184u + 2u)]
+        [TestCase(15u, 15u, 0u, NoT, 224u + 644u + 644u + 168u)]
+        public void Find_OnRightAndBelow__ReturnsCorrectly(uint m, uint n, uint l, uint t, uint expected)
+        {
+            CodeWars.Solutions.BecomeImmortal.BecomeImmortal.Find(m, n, l, t).ShouldBe(expected);
+        }
+
+        [TestCase(8u, 5u, 1u, 100u, 5u)]
+        public void Find__ReturnsCorrectly(uint m, uint n, uint l, uint t, uint expected)
+        {
+            CodeWars.Solutions.BecomeImmortal.BecomeImmortal.Find(m, n, l, t).ShouldBe(expected);
+        }
+    }
+}
---
+++ Source/CodeWars.Solutions/BecomeImmortal/BecomeImmortal.cs
@@ -0,0 +1,51 @@
+﻿/*
+ * https://www.codewars.com/kata/59568be9cc15b57637000054
+ * BECOME IMMORTAL
+ * 
+ * Wartości xor generują w macierzy "munchkin squares".
+ * Koncepcja jest po części opisana tutaj: https://blog.valerauko.net/2018/02/11/munching-the-squares-of-immortality/
+ * 
+ * W maksymalnym kwadracie są wszystkie permutacje wartości, można obliczyć jego wartość jako sumę ciągu arytmetycznego x ilość wierszy (bok kwadratu).
+ * Dalej rekurencyjnie w 3 prostokąty: po prawej, na dole, po skosie na dole (prawo-dół).
+ */
+using System;
+
+namespace CodeWars.Solutions.BecomeImmortal
+{
+    public static class BecomeImmortal
+    {
+        public static ulong Find(uint m, uint n, uint l, uint t) => 
+            CalculateRect(0, m, 0, n, l) % t;
+
+        private static ulong CalculateRect(uint startX, uint widthX, uint startY, uint widthY, uint l)
+        {
+            if ((widthX == 0) || (widthY == 0)) return 0;
+
+            uint largestPowerOf2 = GetLargestPowerOf2(Math.Min(widthX, widthY));
+
+            return SumInSquare(startX ^ startY, largestPowerOf2, l)
+                + CalculateRect(startX + largestPowerOf2, widthX - largestPowerOf2, startY, largestPowerOf2, l)
+                + CalculateRect(startX, largestPowerOf2, startY + largestPowerOf2, widthY - largestPowerOf2, l)
+                + CalculateRect(startX + largestPowerOf2, widthX - largestPowerOf2, startY + largestPowerOf2, widthY - largestPowerOf2, l);
+        }
+
+        private static uint GetLargestPowerOf2(uint x)
+        {
+            uint value = 1u << 31;
+            while (((x & value) == 0) && (value != 0))
+                value >>= 1;
+            return value;
+        }
+
+        private static ulong SumInSquare(uint firstValue, uint width, uint l) => 
+            width * SumSequence(SubtractL(firstValue, l), SubtractL(firstValue + width - 1, l));
+
+        private static uint SubtractL(uint value, uint l) =>
+            value < (2 * l) ? 0 : value - l;
+
+        private static uint SumSequence(uint firstValue, uint lastValue) =>
+            firstValue == lastValue
+            ? firstValue
+            : (lastValue - firstValue + 1) * (firstValue + lastValue) / 2;
+    }
+}
