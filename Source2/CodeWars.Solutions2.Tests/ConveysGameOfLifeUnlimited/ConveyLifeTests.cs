0f4b62b93115a0104b35d5c8086f0c6a955f4f26
minusminus <minusminus@tlen.pl>
2023-04-28 11:43:19 +0200

    - initialized ConwayLife

---
+++ Source2/CodeWars.Solutions2.Tests/ConveysGameOfLifeUnlimited/ConveyLifeTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using CodeWars.Solutions2.ConveysGameOfLifeUnlimited;
+using NUnit.Framework;
+using Shouldly;
+
+namespace CodeWars.Solutions2.Tests.ConveysGameOfLifeUnlimited
+{
+    [TestFixture]
+    public class ConveyLifeTests
+    {
+        [Test]
+        public void SampleKataTest()
+        {
+            int[,] testCase = new int[,] { { 1, 0, 0 }, { 0, 1, 1 }, { 1, 1, 0 } };
+            int[,] expected = new int[,] { { 0, 1, 0 }, { 0, 0, 1 }, { 1, 1, 1 } };
+
+            int[,] res = ConwayLife.GetGeneration(testCase, 1);
+
+            res.ShouldBe(expected);
+        }
+
+        [Test]
+        public void EmptyInput__ReturnsEmpty()
+        {
+            int[,] emptyArray = new int[,] { };
+
+            ConwayLife.GetGeneration(emptyArray, 1).ShouldBe(emptyArray);
+        }
+    }
+}
---
+++ Source2/CodeWars.Solutions2/ConveysGameOfLifeUnlimited/ConwayLife.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace CodeWars.Solutions2.ConveysGameOfLifeUnlimited
+{
+    public class ConwayLife
+    {
+        /* Please note that the htmlize function for C# currently isn't working
+            properly. I tested it on rextester.com and the code worked as expected,
+            but for some reason on codewars it isn't. When I find a solution to
+            the issue I will update the function. */
+        public static int[,] GetGeneration(int[,] cells, int generation)
+        {
+            if (IsEmpty(cells))
+                return new int[,] { };
+
+            return new int[,] { };
+        }
+
+        private static bool IsEmpty(int[,] cells) => 
+            (cells.GetLength(0) * cells.GetLength(1)) > 0;
+    }
+}
